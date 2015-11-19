df6600afe2dba47d52fc2a871fc94188ee2fb0a5
type0 <ghyghygg30@gmail.com>
2015-11-19 20:52:34 +0800

    test2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
-
+//~~~~
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
